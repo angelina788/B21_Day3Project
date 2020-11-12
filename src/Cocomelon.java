546669a024a4610a1c3b30aaf6092ffbf373d08d
angelina788 <limura788@gmail.com>
2020-11-12 11:23:04 -0500

    added three commit

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -3,5 +3,8 @@ public class Cocomelon {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        // today is Thursday
+        // today rainy day
+        // have a fun
     }
 }
