e528dd7a172926fe3d656f650d6b08620e427c68
angelina788 <limura788@gmail.com>
2020-11-12 10:51:10 -0500

    added three line

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Git Last Day");
+        // B21 is exited!!!
+        // B21 is dedicated
+        // B21 is successful testers
     }
 }
